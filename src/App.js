9b2565476530eb7a1d56bd46cb40322a94af5a7e
SrPabaum <mrmster6@gmail.com>
2023-08-30 18:34:59 -0300

    template v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {SlLocationPin} from "react-icons/sl"
+import { AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai"
+import { Link } from 'react-router-dom';
+
+import Logo from "./imgs/logorpadv.jpg"
+import img1 from "./imgs/pf1.png"
+import img2 from "./imgs/pf2.png"
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <h1 className='title'>RP Advocacia 
+         <br></br>e Consultoria Jurídica</h1>
+         <img src={Logo} width={150} alt="RpAdvocacia" title='rp' id='logo'/>
+         <div className='profile'>
+          <div className='container'>
+
+          <div className='bl1'>
+          </div>
+              <p className='text'>
+                Ricardo Pereira Alves
+                <br></br>
+                OAB/MG 153.242
+              </p>
+              <div className='icon-container'>
+              <a className='emrf' href='mailto:rpadvocaciasf@gmail.com'><AiOutlineMail size={30}/></a>
+              <a href='https://api.whatsapp.com/send/?1=pt_BR&phone=553899675176'><AiOutlineWhatsApp size={30}/></a>
+              </div>
+              </div>
+          <div className='container-two'>
+          <div className='bl2'>
+          </div>
+              <p className='textrf'>
+              Raphael Simões de Moraes Neto
+              <br></br> OAB/MG 220.210
+              </p>
+              <div className='iconrf-container'>
+              <a  href='mailto:raphaelnetoadv@gmail.com'><AiOutlineMail size={30}/></a>
+              <a href='https://api.whatsapp.com/send/?1=pt_BR&phone=553899675176'><AiOutlineWhatsApp size={30}/></a>
+              </div>
+         </div> 
+         </div>
+         <div className='rdp'>
+            <SlLocationPin id='loc' size={20}/><a className='loctxt'>Praça Centenária , nº 335,<br/>
+             Centro, São Francisco/MG</a>
+          </div>
+        <element class='header-container'>
+          
+        </element>
       </header>
     </div>
   );
