448fa8ee3f2c59efc5b73b3d071a10656b880f60
SrPabaum <mrmster6@gmail.com>
2023-11-18 20:14:54 -0300

    up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import View from './components/view';
 import Logo from "./imgs/logorpadv.png"
-
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
         <img src={Logo} id='logo' alt='logo' title='logo' width={200} />
         <View/>
+        <Footer/>
     </div>
   );
 }
--- src/components/profiles.js
+++ src/components/profiles.js
@@ -3,6 +3,7 @@ import adv1 from '../imgs/pf1.png';
 import adv2 from '../imgs/pf2.png';
 import { FaWhatsapp } from "react-icons/fa";
 
+
 const Profiles = () => {
   return (
     <div className="profiles">
@@ -10,12 +11,12 @@ const Profiles = () => {
       <div className="profile">
         <img src={adv1} id='adv1' alt='adv1' title='adv1' width={200} />
         <span>Ricardo Alves</span>
-        <FaWhatsapp id="wpp" size={30} />
+        <a href="https://api.whatsapp.com/send/?1=pt_BR&phone=5538999664763"><FaWhatsapp id="wpp" size={30} /></a>
       </div>
       <div className="profile">
         <img src={adv2} id='adv2' alt='logo' title='logo' width={200} />
         <span>Raphael Neto</span>
-        <FaWhatsapp id="wpp" size={30} />
+        <a href="https://api.whatsapp.com/send/?1=pt_BR&phone=553899675176"><FaWhatsapp id="wpp" size={30} /></a>
       </div>
     </div>
   );
